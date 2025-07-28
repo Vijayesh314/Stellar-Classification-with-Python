f68957f4e9247c8927fb52ba7e4c90f3a189eec0
Vijayesh314 <145930677+Vijayesh314@users.noreply.github.com>
2025-07-28 11:38:47 -0700

    Create rfmodel.py

---
+++ rfmodel.py
@@ -0,0 +1,7 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
